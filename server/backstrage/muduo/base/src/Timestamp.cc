e1fd555ae9902460b1b6857195d08c7bd1692979
李少波 <8032361+li-shaobobobobobo@user.noreply.gitee.com>
2021-11-22 16:08:13 +0800

    add code

--- server/backstrage/muduo/base/src/Timestamp.cc
+++ server/backstrage/muduo/base/src/Timestamp.cc
@@ -1,15 +1,9 @@
-// Use of this source code is governed by a BSD-style license
-// that can be found in the License file.
-//
-// Author: Shuo Chen (chenshuo at chenshuo dot com)
-
 #include "Timestamp.h"
 
 #include <sys/time.h>
 #include <stdio.h>
+
 #ifndef __STDC_FORMAT_MACROS
 #define __STDC_FORMAT_MACROS
 #endif
-
-#include <inttypes.h>
 
